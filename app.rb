fb3e0799cf3d71d23f79d6ac667eb17c10d2ad5d
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-05-23 15:52:03 -0400

    Moves code to open and read file to app.rb from views/index.erb

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require 'sinatra'
 require 'erb'
 
 get '/' do
+  @arr = Array.new
+  theFile = File.open("cake.list")
+
+  theFile.each do |line|
+    @arr << line
+  end
+
+  theFile.close
   erb :index
 end
